83f1cd3b370b96ef4cbbc2d9c2e66e23c2a80a5a
Ameya Lokare <lokare.ameya@gmail.com>
2017-10-26 00:27:00 -0700

    WIP: Actual compiling outbound call struct

--- src/calls.rs
+++ src/calls.rs
@@ -7,8 +7,9 @@ use serde_json;
 use chrono::prelude::*;
 use futures::future;
 use futures::{Future, Stream};
-use hyper::{Body, Method, Request};
+use hyper::{Body, Method, Request, Uri};
 use hyper::error::Error;
+use url::{form_urlencoded, Url};
 
 pub struct Calls<'a> {
     client: &'a Client,
@@ -20,6 +21,65 @@ pub struct Call {
     pub account_sid: String,
     pub parent_call_sid: Option<String>,
     #[serde(with = "rfc2822")] pub date_created: DateTime<Utc>,
+}
+
+pub enum TwimlSource<'a> {
+    url(&'a Url),
+    application_sid(&'a str),
+}
+
+pub enum CallbackMethod {
+    post,
+    get,
+}
+
+pub enum StatusCallbackEvent {
+    initiated,
+    ringing,
+    answered,
+    completed,
+}
+
+pub trait IntoUrlEncoded {
+    fn to_url_encoded(&self) -> String;
+}
+
+pub struct OutboundCall<'a> {
+    from: &'a str,
+    to: &'a str,
+    twiml_source: TwimlSource<'a>,
+    method: Option<CallbackMethod>,
+    fallback_url: Option<&'a Url>,
+    fallback_method: Option<CallbackMethod>,
+    status_callback: Option<&'a Url>,
+    status_callback_method: Option<CallbackMethod>,
+    status_callback_event: Vec<StatusCallbackEvent>,
+    send_digits: &'a str,
+}
+
+impl<'a> IntoUrlEncoded for OutboundCall<'a> {
+
+    fn to_url_encoded(&self) -> String {
+        let mut encoder = form_urlencoded::Serializer::new(String::new());
+        encoder.append_pair("From", self.from);
+        encoder.append_pair("To", self.to);
+        let (name, value) = match self.twiml_source {
+            TwimlSource::url(x) => ("Url", x.as_str()),
+            TwimlSource::application_sid(x) => ("ApplicationSid", x),
+        };
+        let _ = match self.method {
+            Some(ref x) => {
+                encoder.append_pair("Method", match *x {
+                    CallbackMethod::post => "POST",
+                    CallbackMethod::get => "GET",
+                });
+                ()
+            }
+            None => ()
+        };
+        encoder.append_pair(name, value);
+        encoder.finish()
+    }
 }
 
 impl<'a> Calls<'a> {
--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ extern crate serde;
 extern crate serde_derive;
 extern crate serde_json;
 extern crate tokio_core;
+extern crate url;
 
 use std::str;
 use std::option::Option;
